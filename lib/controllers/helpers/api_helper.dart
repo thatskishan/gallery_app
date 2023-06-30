812c62b14a70a206c629027ef0a5f9f1d2e2fa4a
kishanvirani <kishanvirani1@gmail.com>
2023-06-30 20:18:45 +0530

    new commit

--- lib/controllers/helpers/api_helper.dart
+++ lib/controllers/helpers/api_helper.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:gallery_app/utils/constants.dart';
 import 'package:http/http.dart' as http;
 
 import '../../models/image_model.dart';
@@ -12,8 +13,7 @@ class ApiHelper {
   Future<ImagesModel?> getImages({
     required String search,
   }) async {
-    String myURL =
-        "https://pixabay.com/api/?key=37976395-53d38e535cf032a48be1109d1&q=$search&image_type=photo&pretty=true";
+    String myURL = apiKey;
     http.Response res = await http.get(Uri.parse(myURL));
 
     if (res.statusCode == 200) {
